66dcc7f8198f6c51f4529137fe3d388ef393b00d
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2021-08-26 00:00:26 -0300

    diferença de if e switchcase

--- SwitchCase.java
+++ SwitchCase.java
@@ -28,6 +28,31 @@ public class SwitchCase {
       System.out.println("Não é um dia da semana!");
     }
 
+    switch (diaDaSemana) {
+      case 1:
+        System.out.println("Domingo");
+        break;
+      case 2:
+        System.out.println("Segunda-Feira");
+        break;
+      case 3:
+        System.out.println("Terça-Feira");
+        break;
+      case 4:
+        System.out.println("Quarta-Feira");
+        break;
+      case 5:
+        System.out.println("Quinta-Feira");
+        break;
+      case 6:
+        System.out.println("Sexta-Feira");
+        break;
+      case 7:
+        System.out.println("Sabado");
+        break;
+      default:
+        System.out.println("Não é um dia da semana válido.");
+        break;
+    }
   }
-
 }

f73940ea1775008eb900e5689e873e2c358c80ba
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2021-08-28 23:05:18 -0300

    correção codigo

--- SwitchCase.java
+++ SwitchCase.java
@@ -10,23 +10,16 @@ public class SwitchCase {
 
     int diaDaSemana = scan.nextInt();
 
-    if (diaDaSemana == 1) {
-      System.out.println("Domingo");
-    } else if (diaDaSemana == 2) {
-      System.out.println("Segunda-Feira");
-    } else if (diaDaSemana == 3) {
-      System.out.println("Terça-Feira");
-    } else if (diaDaSemana == 4) {
-      System.out.println("Quarta-Feira");
-    } else if (diaDaSemana == 5) {
-      System.out.println("Quinta-Feira");
-    } else if (diaDaSemana == 6) {
-      System.out.println("Sexta-Feira");
-    } else if (diaDaSemana == 7) {
-      System.out.println("Sabado");
-    } else {
-      System.out.println("Não é um dia da semana!");
-    }
+    /*
+     * if (diaDaSemana == 1) { System.out.println("Domingo"); } else if (diaDaSemana
+     * == 2) { System.out.println("Segunda-Feira"); } else if (diaDaSemana == 3) {
+     * System.out.println("Terça-Feira"); } else if (diaDaSemana == 4) {
+     * System.out.println("Quarta-Feira"); } else if (diaDaSemana == 5) {
+     * System.out.println("Quinta-Feira"); } else if (diaDaSemana == 6) {
+     * System.out.println("Sexta-Feira"); } else if (diaDaSemana == 7) {
+     * System.out.println("Sabado"); } else {
+     * System.out.println("Não é um dia da semana!"); }
+     */
 
     switch (diaDaSemana) {
       case 1:

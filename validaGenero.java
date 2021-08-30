b97e7512b8ee0bc869e154cf4a274b935a67bfca
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2021-08-30 00:00:26 -0300

    código valida genêro digitando F ou M e indica se está invalido a entrada

---
+++ validaGenero.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class validaGenero {
+  // código valida genêro digitando F ou M e indica se está invalido a entrada
+
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Qual o seu genêro? Masculino ou Feminino? Digite M ou F: ");
+    String genero = scan.next();
+    System.out.println("Seu genero é: " + genero);
+
+    if (genero.equals("M")) {
+      System.out.printf("Seja Bem Vindo!");
+    } else if (genero.equals("m")) {
+      System.out.printf("Seja Bem Vindo!");
+    } else if (genero.equals("F")) {
+      System.out.printf("Seja Bem Vinda!");
+    } else if (genero.equals("f")) {
+      System.out.printf("Seja Bem Vinda!");
+    } else {
+      System.out.println("Não é uma letra que corresponde a um genero válido neste caso!");
+    }
+  }
+}

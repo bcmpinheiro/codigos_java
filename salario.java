7cc9536785a742e5cc4af762d82ce6f9c97b1d49
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2021-09-06 00:33:49 -0300

    código lê salário digitado e aplica aumentos conforme regras

--- salario.java
+++ salario.java
@@ -7,16 +7,21 @@ public class salario {
     Scanner scan = new Scanner(System.in);
 
     System.out.println("Digite o seu salário: ");
-    double salarioAtual = scan.nextDouble();
+    double salarioAtual = scan.nextDouble(); // Double.parseDouble(scan.nextLine());
+    System.out.println("Seu salário atual é: " + salarioAtual);
 
     if (salarioAtual <= 280.00) {
-      System.ou.println(
-          "O salário atual é: " + salarioAtual + " e teve um aumento de 20% que ficou: " + salarioAtual * 0.20);
-    } else if (salario > 280.00 && <= 700.00) {
-      System.ou.println(
-        "O salário atual é: " + salarioAtual + " e teve um aumento de 15% que ficou: " + salarioAtual * 0.15);
+      double novoSalario = salarioAtual + (salarioAtual * 0.20);
+      System.out.println("teve um aumento de 20% que ficou: " + novoSalario);
+    } else if ((salarioAtual > 280.00) && (salarioAtual <= 700.00)) {
+      double novoSalario = salarioAtual + (salarioAtual * 0.15);
+      System.out.println("teve um aumento de 15% que ficou: " + novoSalario);
+    } else if ((salarioAtual > 700.00) && (salarioAtual <= 1500.00)) {
+      double novoSalario = salarioAtual + (salarioAtual * 0.10);
+      System.out.println("teve um aumento de 10% que ficou: " + novoSalario);
+    } else if (salarioAtual > 1500.00) {
+      double novoSalario = salarioAtual + (salarioAtual * 0.05);
+      System.out.println("teve um aumento de 5% que ficou: " + novoSalario);
     }
-
   }
-
 }

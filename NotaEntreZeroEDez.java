abb989356708c52c04a659c55bfbdbad3cdcbe75
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2021-10-05 23:38:21 -0300

    programa pede nota e valida se vai de 0 a 10

---
+++ NotaEntreZeroEDez.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class NotaEntreZeroEDez {
+
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    boolean notaValida = false;
+
+    do {
+      System.out.println("Informe uma nota: ");
+      double nota = scan.nextDouble();
+
+      if (nota >= 0 && nota <= 10) {
+        notaValida = true;
+        System.out.println("Você digitou: " + nota);
+      } else {
+        // notaValida = false;
+        System.out.println("Nota iválida. Digite novamente!");
+      }
+    } while (!notaValida);
+  }
+}

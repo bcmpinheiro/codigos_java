de65681a5a9c50d1fd73037e807042b9ddfc99bd
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2021-10-20 23:10:49 -0300

    codigo faz leitura de populacao e porcentagem de crescimento e devolve na tela

---
+++ Populacao.java
@@ -0,0 +1,19 @@
+
+public class Populacao {
+
+  public static void main(String[] args) {
+
+    int popA = 8000;
+    int popB = 20000;
+    int cont = 0;
+
+    while (popA < popB) {
+      popA += (popA / 100) * 3;
+      popB += (popB / 100) * 1.5;
+      cont++;
+    }
+    System.out.println("População A: " + popA);
+    System.out.println("População B: " + popB);
+    System.out.println("Qtd anos: " + cont);
+  }
+}

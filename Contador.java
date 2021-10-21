0e6d116ebe60907b99b6556b5435fd08284a165f
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2021-10-21 00:03:48 -0300

    programas que contam e devolvem valores na tela

---
+++ Contador.java
@@ -0,0 +1,10 @@
+public class Contador {
+
+  public static void main(String[] args) {
+
+    for (int indice = 0; indice <= 20; indice++) {
+      // System.out.println(indice);
+      System.out.print(indice + " ");
+    }
+  }
+}
---
+++ MaiorNumeroEncontrado.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MaiorNumeroEncontrado {
+
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    int num;
+    int maior = Integer.MIN_VALUE;
+
+    for (int indice = 0; indice < 5; indice++) {
+      System.out.println("Entre com um numero: ");
+      num = scan.nextInt();
+
+      if (num > maior) {
+        maior = num;
+        System.out.println("O numero maior mudou: " + maior);
+      }
+      System.out.println("O maior numero é o: " + maior);
+    }
+  }
+}

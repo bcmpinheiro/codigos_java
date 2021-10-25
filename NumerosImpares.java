ba5aa2ed49c6ca09476a18f6cfdb8943a3da060d
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2021-10-24 21:20:29 -0300

    exercicios

---
+++ NumerosImpares.java
@@ -0,0 +1,12 @@
+public class NumerosImpares {
+
+  public static void main(String[] args) {
+
+    for (int indice = 1; indice < 50; indice++) {
+
+      if (indice % 2 != 0) {
+        System.out.print(indice + " ");
+      }
+    }
+  }
+}

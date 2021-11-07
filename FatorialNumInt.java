5b62fea3216259ca9ec2e0c494fefe06fe7962ad
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2021-11-07 16:50:35 -0300

    programa calcula fatorial de um numero digitado

---
+++ FatorialNumInt.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FatorialNumInt {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Digite um numero: ");
+    int num = scan.nextInt();
+
+    System.out.println("Fatorial de " + num);
+
+    System.out.print(num + "! = ");
+    int fatorial = 1;
+    for (int i = num; i > 1; i--) {
+      fatorial *= i;
+      System.out.print(i + ".");
+    }
+    System.out.println("1 = " + fatorial);
+  }
+}

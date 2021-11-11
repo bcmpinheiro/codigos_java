6791816eb3e2222b733bfae4592eb638d63a1983
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2021-11-11 00:52:18 -0300

    programa mostra numeros primos

---
+++ NumerosPrimos.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class NumerosPrimos {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com um numero: ");
+        int num = scan.nextInt();
+
+        boolean primo = true;
+
+        for(int i = 2; i < num; i++){
+            if (num % i == 0){
+                System.out.println("Não é primo! Divisivel por: " + i);
+                primo = false;
+                //break;
+            }
+        }
+
+        if (primo) {
+            System.out.println("É numero primo!");
+        }
+    }
+}

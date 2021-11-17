095438370fe3c31510360ff67a6c5d7a9801bf3b
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2021-11-16 23:39:07 -0300

    código mostra a tabuada de um numero digitado em uma sequencia escolhida

---
+++ NovoTabuada.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class NovoTabuada {
+
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Entre com o numero para gerar a tabuada: ");
+    int num = scan.nextInt();
+
+    boolean invalido = true;
+    int numInicial, numFinal;
+
+    do {
+      System.out.println("Entre com o ínicio da tabuada");
+      numInicial = scan.nextInt();
+
+      System.out.println("Entre com o final da tabuada");
+      numFinal = scan.nextInt();
+
+      if (numFinal > numInicial) {
+        invalido = false;
+      }
+    } while (invalido);
+
+    System.out.println("Tabuada de " + num + ":");
+    System.out.println("Começar por: " + numInicial);
+    System.out.println("Terminar em:  " + numFinal);
+    System.out.println();
+
+    for (int i = numInicial; i <= numFinal; i++) {
+      System.out.println(num + " x " + i + " = " + (num * i));
+    }
+  }
+}

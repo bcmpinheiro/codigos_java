9087259c3309042be5dbed41473c5e768750b8c4
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2022-01-10 23:58:17 -0300

    Exercicios 01 e 02

---
+++ ClassesMetodos01.java
@@ -0,0 +1,20 @@
+public class ClassesMetodos01 {
+
+  public static void main(String[] args) {
+
+    Lampada01 lampada = new Lampada01();
+
+    lampada.ligar();
+
+    lampada.mostrarEstado();
+
+    lampada.desligar();
+
+    lampada.mostrarEstado();
+
+    lampada.mudarEstado();
+
+    lampada.mostrarEstado();
+
+  }
+}

b6b4277cffdbb2d1363530dd74ea1454f0dc90fa
Barbara <bcmpinheiro@gmail.com>
2023-01-16 23:24:30 -0300

    add classe operacoes e teste

---
+++ src/main/java/aulas/metodos/TesteOperacoes.java
@@ -0,0 +1,9 @@
+package src.main.java.aulas.metodos;
+
+public class TesteOperacoes {
+
+    public static void main(String[] args) {
+        System.out.println("Vai comecar a contagem");
+        System.out.println(Operacoes.contador(1,5));
+    }
+}

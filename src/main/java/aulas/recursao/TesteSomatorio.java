63f54e7e87d68c6f01e63c3f0609dde1f8544805
Barbara <bcmpinheiro@gmail.com>
2023-02-19 23:04:38 -0300

    add classe e teste calculo somatorio

---
+++ src/main/java/aulas/recursao/TesteSomatorio.java
@@ -0,0 +1,9 @@
+package src.main.java.aulas.recursao;
+
+public class TesteSomatorio {
+
+    public static void main(String[] args) {
+
+        System.out.println(Somatorio.somatorio(5));
+    }
+}

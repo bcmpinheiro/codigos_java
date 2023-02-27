f92776009508b06399c95bb1eb292f5352ffd752
Barbara <bcmpinheiro@gmail.com>
2023-02-26 22:51:24 -0300

    ajustes classe e teste ContaBancaria

--- src/main/java/aulas/heranca/ContaPoupanca.java
+++ src/main/java/aulas/heranca/ContaPoupanca.java
@@ -1,14 +1,16 @@
 package src.main.java.aulas.heranca;
 
+import java.util.Calendar;
+
 public class ContaPoupanca extends ContaBancaria {
 
-    private String diaRendimento;
+    private int diaRendimento;
 
-    public String getDiaRendimento() {
+    public int getDiaRendimento() {
         return diaRendimento;
     }
 
-    public void setDiaRendimento(String diaRendimento) {
+    public void setDiaRendimento(int diaRendimento) {
         this.diaRendimento = diaRendimento;
     }
 
@@ -16,4 +18,21 @@ public class ContaPoupanca extends ContaBancaria {
         return calcularNovoSaldo();
     }
 
+    @Override
+    public String toString() {
+        return "ContaPoupanca{" +
+                "diaRendimento='" + diaRendimento + '\'' +
+                '}';
+    }
+
+    public boolean calcularNovoSaldo(double taxaRendimento){
+        Calendar hoje = Calendar.getInstance();
+
+        if(diaRendimento == hoje.get(Calendar.DAY_OF_MONTH)){
+            //saldo += saldo * taxaRendimento;
+            this.setSaldo(this.getSaldo() + (this.getSaldo() * taxaRendimento));
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/aulas/heranca/TesteContas.java
+++ src/main/java/aulas/heranca/TesteContas.java
@@ -7,7 +7,7 @@ public class TesteContas {
         System.out.println("*** Teste ContaBancaria ***");
 
         ContaBancaria contaSimples = new ContaBancaria();
-        contaSimples.setNomeCliente("Cliente Conta Simples");
+        contaSimples.setNomeCliente("Cliente Conta Bancaria");
         contaSimples.setNumConta("11111");
         contaSimples.setSaldo(0);
 
@@ -19,9 +19,43 @@ public class TesteContas {
 
         System.out.println(contaSimples);
 
+        System.out.println("*** Teste ContaPoupanca ***");
 
+        ContaPoupanca contaPoupanca = new ContaPoupanca();
+        contaPoupanca.setNomeCliente("Cliente Conta Poupanca");
+        contaPoupanca.setNumConta("22222");
+        contaPoupanca.setSaldo(0);
+        contaPoupanca.setDiaRendimento(25);
 
+        contaPoupanca.depositar(100.00);
 
+        realizarSaque(contaPoupanca,50.00);
+
+        realizarSaque(contaPoupanca,70.00);
+
+        if(contaPoupanca.calcularNovoSaldo(0.5)){
+            System.out.println("Novo rendimento aplicado, novo saldo e de = " + contaPoupanca.getSaldo());
+        } else {
+            System.out.println("Hoje nao e dia de rendimento, novo saldo nao calculado.");
+        }
+        System.out.println(contaPoupanca);
+
+        System.out.println("*** Teste ContaEspecial ***");
+
+        ContaEspecial contaEspecial = new ContaEspecial();
+        contaEspecial.setNomeCliente("Cliente Conta Especial");
+        contaEspecial.setNumConta("33333");
+        contaEspecial.setLimite(50);
+
+        contaEspecial.depositar(100.00);
+
+        realizarSaque(contaEspecial,50.00);
+
+        realizarSaque(contaEspecial,70.00);
+
+        realizarSaque(contaEspecial,80.00);
+
+        System.out.println(contaEspecial);
     }
 
     private static void realizarSaque(ContaBancaria conta, double valor){
